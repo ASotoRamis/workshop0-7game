a0d25a8f9b932a9d840e481ece2a233e669ea5d7
Alberto Soto Fernández <alberto.soto@gmail.com>
2015-10-02 20:22:10 +0200

    DWEC-W0 Initial setup

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param txt
+ */
+function setData(txt){
+
+}
+/**
+ *
+ */
+function getSelectedData(){
+
+}
+/**
+ *
+ * @param item
+ */
+function isValid(item){
+
+}
+/**
+ *
+ * @param param1
+ */
+function doActionName(param1){
+
+}
